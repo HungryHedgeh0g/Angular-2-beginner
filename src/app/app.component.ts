835f54dd4a19fc1fb3b05193166534434f24219c
HungryHedgeh0g <sonic-europe@yandex.by>
2022-07-14 18:38:11 +0300

    smth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
 
+const todos = [
+  { title: "Проснуться", completed: true },
+  { title: "Умыться", completed: true },
+  { title: "Завтрак", completed: true },
+  { title: "Пойти на работу", completed: true },
+  { title: "Обед", completed: true },
+  { title: "Уйти с работы", completed: true },
+  { title: "Ужин", completed: true },
+  { title: "Поехать на курсы", completed: true },
+  { title: "Поехать домой", completed: false },
+  { title: "Съесть яблоко", completed: false },
+  { title: "Умыться", completed: false },
+  { title: "Заснуть", completed: false }
+]
+
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
 export class AppComponent {
-  title = 'proj_1';
+  title = 'Angular 2';
+  todos = todos;
+
+  toggle(todo: any) {
+    todo.completed = !todo.completed;
+  }
+
+  delete(todo: any) {
+    let index = this.todos.indexOf(todo);
+    if (index > -1) {
+      this.todos.splice(index, 1);
+    }
+  }
 }
